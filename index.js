55481c619460f0cca14495aeaf201afa14b7a88a
kerwinteh <kerwintehkerwei@gmail.com>
2023-03-08 12:52:10 -0500

    edit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function (){
-    $("#steak").hide();
-    $("#steak").fadeIn(2000);
-    $(".display").hide();
-    $(".display").fadeIn(2000);
+    let steak = $("#steak");
+    let display = $(".display");
+    steak.hide();
+    steak.fadeIn(2000);
+    display.hide();
+    display.fadeIn(2000);
 })
